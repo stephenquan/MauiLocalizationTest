dca7421a308e633def0defe1e5bdd663636a4806
Stephen Quan <squan@esri.com>
2024-08-05 15:57:56 +1000

    Initial check in

---
+++ MauiLocalizationTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiLocalizationTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiLocalizationTest/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MauiLocalizationTest.Resources.Strings;
+
+namespace MauiLocalizationTest
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public string LBL_HELLO_WORLD => AppStrings.LBL_HELLO_WORLD;
+        public string LBL_WELCOME => AppStrings.LBL_WELCOME;
+        public string BTN_CLICK_ME => AppStrings.BTN_CLICK_ME;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = string.Format(AppStrings.BTN_CLICKED_1_TIME, count);
+            else
+                CounterBtn.Text = string.Format(AppStrings.BTN_CLICKED_N_TIMES, count);
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
